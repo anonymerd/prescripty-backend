3573992b4cfede844b6ce5aa31030c93be523fe7
Rohit Bisht <Rohit.Bisht@BRILLIO.COM>
2022-09-14 22:23:20 +0530

    Added CORS support

--- src/main/java/com/brillio/prescripty/controller/DoctorController.java
+++ src/main/java/com/brillio/prescripty/controller/DoctorController.java
@@ -2,6 +2,7 @@ package com.brillio.prescripty.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.brillio.prescripty.entity.Doctor;
 import com.brillio.prescripty.service.DoctorService;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/api")
 public class DoctorController {
 	
--- src/main/java/com/brillio/prescripty/controller/PatientController.java
+++ src/main/java/com/brillio/prescripty/controller/PatientController.java
@@ -3,6 +3,7 @@ package com.brillio.prescripty.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.brillio.prescripty.entity.Patient;
 import com.brillio.prescripty.service.PatientService;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/api")
 public class PatientController {
 	// Defining field for Patient Service.
